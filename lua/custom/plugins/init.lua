0e6ac0b300c790bfaa7167945a48bdf91fac10d8
LEMA EF <desenvolvedores@lemaef.com.br>
2024-05-08 09:58:01 -0300

    adição de plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'f-person/git-blame.nvim',
+    config = function()
+      vim.g.gitblame_date_format = '%r (%d %b %Y)'
+      vim.g.gitblame_message_template = '<author> • <date>'
+
+      vim.keymap.set('n', '<Leader>go', ':GitBlameOpenCommitURL<CR>', { noremap = true, silent = true, desc = 'Open commit URL' })
+    end,
+  },
+}
